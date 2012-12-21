ac95ae1d441f0a754fd4c16bbd9504c68ab5bdf3
niaeashes <nia.eashes@gmail.com>
2012-12-21 16:24:14 +0900

    Create LazyBuilder_Taxonomy::add() and remove() method.
    commit for #12

--- lazybuilder/taxonomy.php
+++ lazybuilder/taxonomy.php
@@ -4,11 +4,35 @@ class LazyBuilder_Taxonomy
 {
 	public function add($taxonomy, array $terms)
 	{
-		
+		$term_default = array(
+			'description' => '',
+			'parent' => 0,
+			'slug'
+		);
+
+		foreach ( $terms as $slug => $term_params )
+		{
+			$term = array_merge($term_default, $term_params, array('slug' => $slug));
+			$name = $term['name'];
+			unset($term['name']);
+
+			wp_insert_term($name, $taxonomy, $term);
+		}
 	}
 
 	public function remove($taxonomy, array $terms)
 	{
-		
+		foreach ( $terms as $term_identify )
+		{
+			if ( is_string($term_identify) ) {
+				$term_id = get_term_by('slug', $term_identify, $taxonomy)->term_id;
+			}
+
+			if ( is_int($term_identify) ) {
+				$term_id = $term_identify;
+			}
+
+			wp_delete_term($term_id, $taxonomy);
+		}
 	}
 }
