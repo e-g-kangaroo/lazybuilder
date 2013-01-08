ad42483fb5fa7758ff8657b02934f532c89b9024
e-g-kangaroo <e.g.kangaroo@gmail.com>
2013-01-08 15:56:01 +0900

    create example builder.

--- buildings/001-examples.php
+++ buildings/001-examples.php
@@ -1,19 +1,52 @@
 <?php
-class Building_Test {
+class Building_Examples {
 
 	public function up() {
 
 		LazyBuilder_Taxonomy::add('category', array(
-			'cat_a' => array(
-				'name' => 'なまえ',
-			)
+			'slug_a' => array(
+				'name' => 'category a',
+			),
+			'slug_b' => array(
+				'name' => 'category b',
+				'description' => 'this category is lazybuilder test.',
+			),
+			'slug_c' => array(
+				'name' => 'category c',
+			),
 		));
+
+		LazyBuilder_Taxonomy::modify('category', array(
+			'slug_a' => array(
+				'name' => 'modify',
+			),
+		));
+
+		LazyBuilder_Taxonomy::remove('category', array(
+			'slug_d', 'slug_e',
+		));
+
 	}
 	
 	public function down() {
 
+		LazyBuilder_Taxonomy::add('category', array(
+			'slug_d' => array(
+				'name' => 'category d',
+			),
+			'slug_e' => array(
+				'name' => 'category e',
+			),
+		));
+
+		LazyBuilder_Taxonomy::modify('category', array(
+			'slug_a' => array(
+				'name' => 'category a',
+			),
+		));
+
 		LazyBuilder_Taxonomy::remove('category', array(
-			'cat_a'
+			'slub_a', 'slug_b', 'slug_c'
 		));
 	}
 }
